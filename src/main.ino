62b2ec8c8ed9a02c95f9d599ac4d02ac7b610d45
rahuldeo2047 <rahuldeo2047@gmail.com>
2017-06-26 14:48:18 +0530

    Issue #1 [envsetup] version.h test

--- src/main.ino
+++ src/main.ino
@@ -4,20 +4,30 @@
 // EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
 
 #include <Arduino.h>
-
-#include "EmonLib.h"                   // Include Emon Library
-#include "LinkedList.h"
 #include "config.h"
 #include "version.h"
 
-//#if !defined(__AVR_ATmega2560__)
+#include "elapsedMillis.h"
 
-//#error jhhgkj
-//#
+elapsedMillis telegramBotMsgUpdateTime;
+elapsedMillis telegramBotHeartbeatTime;
+elapsedMillis analogSampleUpdateTime;
+
+#include "EmonLib.h"                   // Include Emon Library
+#include "LinkedList.h"
+
+EnergyMonitor emon1;                   // Create an instances
 
 
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
+
+const char * host = "google.co.in";
+const int httpPort = 80;
+WiFiClient wClient;
+unsigned long lastDisconnectionTs = 0;
+uint8_t lastWifiConnectivityStatus = 0;
+
 
 #include <ESP8266TelegramBOT.h>
 
@@ -35,27 +45,17 @@ long Bot_lasttime;   //last time messages' scan has been done
 int Bot_hb = 3600000; //mean time between scan messages
 long Bot_hb_lasttime;   //last time messages' scan has been done
 
-
-EnergyMonitor emon1;                   // Create an instances
-
-
-//unsigned long
-double totalPower = 0;
-
-const char * host = "google.co.in";
-const int httpPort = 80;
-WiFiClient wClient;
-unsigned long lastDisconnectionTs = 0;
-uint8_t lastWifiConnectivityStatus = 0;
-
 bool hasSentLast = false;
-//WiFiUDP Udp;
 
-//unsigned int localUdpPort = 10000;
-//char incomingPacket[255] = {0};
+
 
 void wifi_setup()
 {
+
+ //
+ //Connect to WiFi
+ //
+ //
 
   Serial.printf("Connecting to %s ", WIFISSID);
   WiFi.begin(WIFISSID, PASSWORD);
@@ -68,7 +68,12 @@ void wifi_setup()
   Serial.print(" connected to wifi.\nChecking internet connectivity ..");
 
 
-  //wClient.connect(host, httpPort);
+  //
+  //Check net connectivity a dirty way
+  //
+  //
+
+  bot.begin();      // launch Bot functionalities
 
   while(!bot.sendMessage(telegramGroupID, "Hi I am Powered ON and connected to the internet Just NOW ( "
   +String(millis()/1000.0) +" seconds ) ", "")) // Test
@@ -77,12 +82,8 @@ void wifi_setup()
     Serial.print(".");
     //
   }
-  //wClient.stop();
   hasSentLast = true;
   Serial.println(" checked all fine.");
-
-  //Udp.begin(localUdpPort);
-  //Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
 
 }
 
@@ -120,6 +121,8 @@ void Bot_EchoMessages(bool status, double data) {
         bot.sendMessage(bot.message[i][4], "On change status has been disabled", "");
       }
     }
+
+
     if (bot.message[i][5] == "watermotorstatus") {
       Serial.println("BOT : CMD rec : watermotorstatus");
       //digitalWrite(13, LOW);    // turn the LED off (LOW is the voltage level)
@@ -132,20 +135,26 @@ void Bot_EchoMessages(bool status, double data) {
         bot.sendMessage(bot.message[i][4], String("Water meter is OFF with current factor "+String(data)+"mA"), "");
       }
     }
+
+
     if (bot.message[i][5] == "uptime") {
       Serial.println("BOT : CMD rec : uptime");
-      float ts = millis();
-      String duration = timeScale(&ts);
+      //float ts = millis();
+      String duration  = timeFormat(millis()); //= timeScale(&ts);
 
-      bot.sendMessage(bot.message[i][4], String("uptime : "+duration), "");
+      bot.sendMessage(bot.message[i][4], String("uptime : \u2705 "+duration), "");
 
     }
+
+
     if (bot.message[i][5] == "version") {
-        Serial.println("BOT : CMD rec : version");
+      Serial.println("BOT : CMD rec : version");
 
-        bot.sendMessage(bot.message[i][4], (_VER_), "");
+      bot.sendMessage(bot.message[i][4], (_VER_), "");
 
-      }
+    }
+
+
     if (bot.message[i][5] == "start") {
       Serial.println("BOT : Started");
       String wellcome = "Wellcome from Water-Electric-Motor, your personal Bot";
@@ -165,102 +174,20 @@ void Bot_EchoMessages(bool status, double data) {
 }
 
 
-String timeScale(float *timediff)
+String timeFormat(unsigned long ts)
 {
-  //float timediff = *_timediff;
-  String durationStr = "";
-  bool lastTimeMultiplierChecked = true;
-
-  if(*timediff >= 1000 && lastTimeMultiplierChecked)
-  {
-
-    *timediff /= 1000; // seconds
-    durationStr = String(*timediff);
-    durationStr += " s ";
-
-    //Serial.print(" timediff seconds: ");
-    //Serial.print(timediff);
-  }
-  else
-  {
-    lastTimeMultiplierChecked = false;
-  }
-
-  if(*timediff >= 60 && lastTimeMultiplierChecked)
-  {
-    *timediff /= 60; // minutes
-    durationStr = String(*timediff);
-    durationStr += " m ";
-
-    //Serial.print(" timediff minutes: ");
-    //Serial.print(timediff);
-
-  }
-  else
-  {
-    lastTimeMultiplierChecked = false;
-  }
-
-
-
-  if(*timediff >= 60 && lastTimeMultiplierChecked)
-  {
-    *timediff /= 60; // hours
-    durationStr = String(*timediff);
-    durationStr += " h ";
-
-    // Serial.print(" timediff hours: ");
-    // Serial.print(timediff);
-  }
-  else
-  {
-    lastTimeMultiplierChecked = false;
-  }
-
-
-  if(*timediff >= 24 && lastTimeMultiplierChecked)
-  {
-    *timediff /= 24; // days
-    durationStr = String(*timediff);
-    durationStr += " d ";
-
-    // Serial.print(" timediff days: ");
-    // Serial.print(timediff);
-  }
-  else
-  {
-    lastTimeMultiplierChecked = false; // :P
-  }
-
-  return durationStr;
-
-
+  unsigned long now = ts/1000.0;
+  int seconds = now%60;
+  int minutes = (now/60)%60;
+  int hours   = (now/3600)%24;
+  int days    = (now/(3600*24)); // Not changing to month further
+  return String(
+    String(days) + " d, "
+    + String(hours) + " h, "
+    + String(minutes) + " m, "
+    + String(seconds) + " s"
+  );
 }
-
-void _Bot_EchoMessages() {
-  Serial.println("MSG :");
-  for (int i = 1; i < bot.message[0][0].toInt() + 1; i++)      {
-
-    Serial.print(bot.message[i][0]);
-    Serial.print(" > ");
-    Serial.print(bot.message[i][1]);
-    Serial.print(" > ");
-    Serial.print(bot.message[i][2]);
-    Serial.print(" > ");
-    Serial.print(bot.message[i][3]);
-    Serial.print(" > ");
-    Serial.print(bot.message[i][4]);
-    Serial.print(" > ");
-    Serial.println(bot.message[i][5]);
-
-    bot.sendMessage(bot.message[i][4], bot.message[i][5], "");
-  }
-  bot.message[0][0] = "";   // All messages have been replied - reset new messages
-}
-
-
-//#endif // !defined(__AVR_ATmega2560__)
-
 
 void setup()
 {
@@ -269,22 +196,18 @@ void setup()
   Serial.println("Ready");
   delay(1000);
 
-  //#if !defined(__AVR_ATmega2560__)
 
   wifi_setup();
 
-  //Serial.println("");
-  //Serial.println("WiFi connected");
   Serial.println("IP address: ");
   IPAddress ip = WiFi.localIP();
   Serial.println(ip);
 
   bot.begin();      // launch Bot functionalities
 
-  //#endif // !defined(__AVR_ATmega2560__)
-
   emon1.current(A0, 60.6);// 111.1);             // Current: input pin, calibration.
 
+  emon1.calcIrms(1480);
   emon1.calcIrms(1480);
   emon1.calcIrms(1480);
 
@@ -299,266 +222,168 @@ void setup()
   // emon1.calcIrms(1480);
   // Remove few initial fuctuations
 
-
-
 }
 
-
-float j = 0;
 
 void loop()
 {
 
-
-  static unsigned long lastTime = 0;
+  //
+  //Read analog data as per
+  // https://learn.openenergymonitor.org/electricity-monitoring/ct-sensors/interface-with-arduino
+  //
 
   const  double alpha = 0.98;
-  static double totalDeltaTime = 0;
-  static double totalTime = 0;
-  static double IrmskWh = 0;
   double Irms = 0;
   double power = 0;
-  double deltaTime = 0;
-  static double IrmsAvg = 0;
 
-  totalTime = (double)(millis())/3600000.0;///(1000*60*60);
-
-  //emon1.analogDataList->clear();
-
-  Irms = emon1.calcIrms(1480 );  // Calculate Irms only
-  //int sampleCount = 1000;
-
-  //Serial.print("0000000000000000");
-  //Serial.println("00");//emon1.analogDataList->size());
-
-  // while(--sampleCount > 0)
-  // {
-  //
-  //   Serial.println(emon1.analogDataList->shift());
-  // }
-
-  power = Irms*230.0;
-
-  if(Irms>0.20)  // probably ignore accounting anything below 200mA
+  if(analogSampleUpdateTime > 20)
   {
+    analogSampleUpdateTime = 0;
+    Irms = emon1.calcIrms(1480 );  // Calculate Irms only
 
-    if(Irms>2.0)
+    power = Irms*230.0;
+
+    if(Irms>0.20)  // probably ignore accounting anything below 200mA
     {
-      whetherMotorIsOn = true;
+
+      if(Irms>2.0)
+      {
+        whetherMotorIsOn = true;
+      }
+
     }
 
-    deltaTime = ( totalTime - lastTime );
+    if(Irms<0.20)
+    {
+      whetherMotorIsOn = false;
+    }
 
-    totalDeltaTime += deltaTime; // required due to if(Irms>0.20)
-
-    IrmskWh += (power * totalDeltaTime) / 3600000.0 / 1000.0;
-
+    Serial.println(Irms);
   }
 
-  if(Irms<0.20)
+  //
+  //Get telegram updates
+  // https://core.telegram.org/bots/api
+  //
+
+  if(telegramBotMsgUpdateTime > Bot_mtbs)
   {
-    whetherMotorIsOn = false;
-  }
+    static int discCnt = 0;
+    telegramBotMsgUpdateTime = 0;
 
-  Serial.println(Irms);
+    //
+    // Keep on checking is WiFi is connected
+    //
+    //
 
-  lastTime = totalTime;
-
-  ///#if !defined(__AVR_ATmega2560__)
-
-  static int discCnt = 0;
-
-  if (millis() > Bot_lasttime + Bot_mtbs)  {
-
-    // check internet connectivity
-
-    if (WiFi.status() != WL_CONNECTED) //!wClient.connect(host, httpPort))
+    if (WiFi.status() != WL_CONNECTED)
     {
       Serial.println("Internet connectivity failed");
-      //wClient.stop();
+
       hasSentLast = false;
 
-      // if(lastWifiConnectivityStatus != wClient.connected()) // since passed the last return status has to return connected status here
-      // {
       lastDisconnectionTs = millis();
-      //   lastWifiConnectivityStatus = wClient.connected();
-      // }
       discCnt++;
       delay(5000);
       return;
     }
-    //wClient.stop();
 
-    // while (WiFi.status() != WL_CONNECTED)
-    // {
-    //   delay(500);
-    //   Serial.print(".");
-    // }
     //
-    // Serial.print(" connected to wifi.\nChecking internet connectivity ..");
-
-
-
+    //If WiFi is connected check for internet
+    //
+    //
     if(!hasSentLast)
     {
 
       hasSentLast = bot.sendMessage(telegramDebugID
-      , String( "Hi I am connected back to the internet after ( "
-      + String( (discCnt*5000.0)/1000.0 )
-      + " plus ) "
-      + String( (millis() - lastDisconnectionTs ) /1000.0)
-      +" seconds ago."), ""); // Test
+        , String( "Hi I am connected back to the internet after ( "
+        + String( (discCnt*5000.0)/1000.0 )
+        + " plus ) "
+        + String( (millis() - lastDisconnectionTs ) /1000.0)
+        +" seconds ago."), ""); // Test
 
-      if(!hasSentLast)
-      {
-        delay(5000);
-        return;
+        if(!hasSentLast)
+        {
+          delay(5000);
+          return;
+        }
+
       }
 
-      //lastDisconnectionTs = millis();
-    }
 
+      bot.getUpdates(bot.message[0][1]);   // launch API GetUpdates up to xxx message
+      Bot_EchoMessages(whetherMotorIsOn, Irms);   // reply to message with Echo
 
-    bot.getUpdates(bot.message[0][1]);   // launch API GetUpdates up to xxx message
-    Bot_EchoMessages(whetherMotorIsOn, Irms);   // reply to message with Echo
-
-    if(autoStatus || Started)
-    {
-      // char str[50];
       //
-      // if(Irms>0.20)  // probably ignore accounting anything below 200mA
-      // {
-      //   sprintf(str, "Data : %f", Irms);
-      // }
-      // else
-      // {
-      //   sprintf(str, "Data : 0.0", Irms);
+      //Check if autoStatus sending is 'ON' or /start msg has been sent here
+      //
+      //
 
-      // }
-
-      if(whetherMotorIsOn != whetherMotorIsOnLast)
+      if(autoStatus || Started)
       {
-        whetherMotorIsOnLast = whetherMotorIsOn;
-        String duration = " ";
-        String str = " ";
-        float timediff =  millis()-lastMotorStartedTimeStamp ;
-        lastMotorStartedTimeStamp = millis();
-        duration = String(timediff);
-        duration += " ms ";
-        bool lastTimeMultiplierChecked = true;
+        //
+        //If motor running status changed send msg automatically by default
+        //
+        //
 
-
-        if(whetherMotorIsOn)
+        if(whetherMotorIsOn != whetherMotorIsOnLast)
         {
-          str = "Water motor is ON now. It was 'off' for ";
+          whetherMotorIsOnLast = whetherMotorIsOn;
+          String duration = " ";
+          String str = " ";
+          float timediff =  millis()-lastMotorStartedTimeStamp ;
+          lastMotorStartedTimeStamp = millis();
+          duration = String(timediff);
+          duration += " ms ";
+          bool lastTimeMultiplierChecked = true;
+
+
+          if(whetherMotorIsOn)
+          {
+            str = "Water motor is ON now. It was 'off' for ";
+          }
+          else
+          {
+            str = "Water motor is OFF now. It was 'on' (or another state) for ";
+          }
+
+          duration = timeFormat(timediff); //timeScale(&timediff);
+
+          Serial.print("timediff ms : ");
+          Serial.print(timediff);
+
+
+          if(whetherMotorIsOn)
+          {
+            bot.sendMessage(teleBotMsgID, String(str+duration), "");
+          }
+          else
+          {
+            bot.sendMessage(teleBotMsgID, String(str+duration), "");
+          }
+
         }
-        else
-        {
-          str = "Water motor is OFF now. It was 'on' (or another state) for ";
+
+        //
+        //SEND HB MSG TO a debug telegram user
+        //
+        //
+
+        if(telegramBotHeartbeatTime > Bot_hb) {
+          telegramBotHeartbeatTime = 0;
+          //if (millis() > Bot_hb_lasttime + Bot_hb)  {
+          //float ts = millis();
+          String duration = timeFormat(millis()); // timeScale(&ts);
+
+          bot.sendMessage(telegramDebugID, String("hb \u2764\ufe0f : "+ duration +" "+Irms+" mA."), "");
+
+          Bot_hb_lasttime = millis();
         }
-
-        duration = timeScale(&timediff);
-
-        Serial.print("timediff ms : ");
-        Serial.print(timediff);
-
-
-        if(whetherMotorIsOn)
-        {
-          bot.sendMessage(teleBotMsgID, String(str+duration), "");
-        }
-        else
-        {
-          bot.sendMessage(teleBotMsgID, String(str+duration), "");
-        }
-
-        // if(timediff >= 1000 && lastTimeMultiplierChecked)
-        // {
-        //
-        //   timediff /= 1000; // seconds
-        //   duration = String(timediff);
-        //   duration += " s ";
-        //
-        //   Serial.print(" timediff seconds: ");
-        //   Serial.print(timediff);
-        // }
-        // else
-        // {
-        //   lastTimeMultiplierChecked = false;
-        // }
-        //
-        // if(timediff >= 60 && lastTimeMultiplierChecked)
-        // {
-        //   timediff /= 60; // minutes
-        //   duration = String(timediff);
-        //   duration += " m ";
-        //
-        //   Serial.print(" timediff minutes: ");
-        //   Serial.print(timediff);
-        //
-        // }
-        // else
-        // {
-        //   lastTimeMultiplierChecked = false;
-        // }
-        //
-        //
-        //
-        // if(timediff >= 60 && lastTimeMultiplierChecked)
-        // {
-        //   timediff /= 60; // hours
-        //   duration = String(timediff);
-        //   duration += " h ";
-        //
-        //   Serial.print(" timediff hours: ");
-        //   Serial.print(timediff);
-        // }
-        // else
-        // {
-        //   lastTimeMultiplierChecked = false;
-        // }
-        //
-        //
-        // if(timediff >= 24 && lastTimeMultiplierChecked)
-        // {
-        //   timediff /= 24; // days
-        //   duration = String(timediff);
-        //   duration += " d ";
-        //
-        //   Serial.print(" timediff days: ");
-        //   Serial.print(timediff);
-        // }
-        // else
-        // {
-        //   lastTimeMultiplierChecked = false; // :P
-        // }
-        //
-
 
       }
 
-      if (millis() > Bot_hb_lasttime + Bot_hb)  {
-        float ts = millis();
-        String duration = timeScale(&ts);
-
-        bot.sendMessage(telegramDebugID, String("hb : "+ duration +" "+Irms+" mA."), "");
-
-        Bot_hb_lasttime = millis();
-      }
-
+      Bot_lasttime = millis();
     }
 
-    Bot_lasttime = millis();
   }
-
-
-  //bot.sendMessage(bot.message[i][4], String("Water meter is OFF with current factor "+String(data)+"mA"), "");
-  // hb
-
-  //#endif // !defined(__AVR_ATmega2560__)
-
-
-
-
-
-}
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define _VER_ ( "v0.0.0-0-gefc0b0e" )
+#define _VER_ ( "v0.0.0-2-ge8b3023" )
 
 #endif //VERSION_H
