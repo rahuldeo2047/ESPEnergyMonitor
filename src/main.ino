7a954fe3c5198d1740617eede9106beab8b1787a
Rahul Deo <rahul@ggn000220-lnx.greyorange.com>
2018-06-13 23:41:36 +0530

    buildable code, to test

--- src/main.ino
+++ src/main.ino
@@ -6,10 +6,12 @@
 #include <Arduino.h>
 #include "config.h"
 //#include "version.h"
-#include "data.h"
 #include <ESP8266WiFi.h>
 
-#include <ESP8266TrueRandom.h>
+#include "RemoteDebug.h"
+RemoteDebug Debug;
+
+//#include <ESP8266TrueRandom.h>
 
 #ifdef ESP8266
 extern "C" {
@@ -19,143 +21,49 @@ extern "C" {
 
 #include "elapsedMillis.h"
 
+const char * HOST_NAME="remotedebug-air_conditioner_energy";
 
 elapsedSeconds testMotorStatusChange;
-unsigned long testTime = 0;
 
-bool isWifiConnected = false;
+unsigned long last_time_thingspoke, last_time_telnet_talked ;
+const int updateTelnetInterval = 1 * 1000;
 
-unsigned long lastDisconnectionTs = 0;
-unsigned long lastMotorStatusChangeTs = 0;
+WiFiClient client;
 
-void wifi_setup()
-{
 
-  //
-  //Connect to WiFi
-  //
-  //
-
-  int retries = 10;
-  Serial.printf("Connecting to %s ", WIFISSID);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFISSID, PASSWORD);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-    retries--;
-    if(0==retries)
-    {
-      Serial.printf("Maximum retries reached, restarting ...");
-      ESP.restart();
-    }
-  }
-  isWifiConnected = true;
-
-  Serial.print(" connected to wifi.");//\nChecking internet connectivity ..");
-
-  checkInternetConnection();
-
-}
-
-gScript_motor_status motor_status_gScript = UNKNOWN;
-gScript_motor_status last_motor_status_gScript = UNKNOWN;
+bool whether_post_wifi_connect_setup_done;
 
 void setup()
 {
   Serial.begin(115200);
-  delay(2000);
-  Serial.println("Ready");
-  delay(1000);
-  Serial.printf("Version %s\n",_VER_);
+  WiFi.disconnect();
+  delay(10);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  whether_post_wifi_connect_setup_done = false;
+
+  //Serial.printf("Version %s\n",_VER_);
   Serial.printf("Build at %s %s\n", __DATE__, __TIME__);
   Serial.print("MAC: "); Serial.println(WiFi.macAddress());
 
   double Irms = currentSample_Init();
 
-  wifi_setup();
-  delay(2000);
-
-  Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
-
-
   bool state ;
 
-  #if !defined(TEST_MODE)
-
-  //=  -1;//
   state =  curretSample_Loop(&Irms);
   state =  curretSample_Loop(&Irms);
   state =  curretSample_Loop(&Irms);
   state =  curretSample_Loop(&Irms);
   state =  curretSample_Loop(&Irms);
 
-  #else
-  //  if(testMotorStatusChange > testTime)
-  {
-    testMotorStatusChange = 0;
-    testTime = 0;//ESP8266TrueRandom.random(11,19);
-
-    bool status;
-    //gScript_type = hb;
-    Irms = ESP8266TrueRandom.random(10,600)/100.0;
-
-    if(Irms>current_factor_threshold_for_motor_ON)
-    {
-      status = true ; //signifies motor is ON
-    }
-
-    if(Irms<current_factor_threshold_for_motor_ON)
-    {
-
-      status = false ; //signifies motor is OFF
-    }
-
-    state = status;
-
-    //state = (bool)ESP8266TrueRandom.randomBit();
-
-
-    Serial.print("Random test ts:");
-    Serial.print(testTime+millis());
-    Serial.print(" test Irms: ");
-    Serial.println(Irms);
-  }
-
-  #endif // !defined(TEST_MODE)
-
-
-
-  motor_status_gScript = UNKNOWN;
-
-  if(state)
-  {
-    motor_status_gScript = ON;
-  }
-  else
-  {
-    motor_status_gScript = OFF;
-  }
-
-  //googlespreadsheet_Init(motor_status_gScript, Irms, powerOn);
-  phpgsheet_Init(motor_status_gScript, Irms, powerOn);
-  last_motor_status_gScript = motor_status_gScript;
-
-
-  Serial.print(" connected, sync with millis()...");
-  timesync();
-  delay(5000);
-  Serial.println("Done");
-
-  motor_status_gScript = UNKNOWN;
-  last_motor_status_gScript = UNKNOWN;
-
-
-//while(1)
-//ESP.wdtFeed();
 }
 
 double approxRollingAverage (double avg, double new_sample) {
@@ -169,169 +77,86 @@ double approxRollingAverage (double avg, double new_sample) {
 bool last_state = false;
 void loop()
 {
-  gScript_type gScript_type = unknown;
-  //gScript_motor_status motor_status_gScript = UNKNOWN;
-  unsigned long ts = 0;
-  //return;
 
-  // Only valid if we are sending data within 10 seconds
-  if (WiFi.status() != WL_CONNECTED)
-  {
+   double Irms ;
 
-    if(isWifiConnected)// && (WiFi.status() != WL_CONNECTED))
+    if(false==whether_post_wifi_connect_setup_done)
     {
-
-      lastDisconnectionTs = millis();
-      Serial.print(millis());
-      Serial.print(" : ");
-      Serial.println("WiFi connectivity failed");
-
-
-      isWifiConnected = false;
-
-      //wifiDisconnectionTime = 0;
+      if(WiFi.status() == WL_CONNECTED)
+      {
+        setup_OTA();
+        rd_setup(HOST_NAME);
+        whether_post_wifi_connect_setup_done = true;
+      }
     }
-    Serial.print(".");
-    //discCnt++;
-    delay(500);
-    return;
-  }
-
-
-
-
-  //
-  // All power state calculation
-  //
-  //
-
-  // these are static only for test purpose
-
-  #if !defined(TEST_MODE)
-
-  double Irms = -1.0;
-  if(!isWifiConnected)
-  {
-    Serial.println();
-  }
-  bool state = curretSample_Loop(&Irms);
-  state = curretSample_Loop(&Irms);
-  state = curretSample_Loop(&Irms);
-  state = curretSample_Loop(&Irms);
-
-  #else
-
-  static double Irms = -1.0;
-  static bool state = 0;
-
-  if(
-    (testMotorStatusChange > testTime)
-    ||
-    ( UNKNOWN == motor_status_gScript )
-  )
-  {
-    testMotorStatusChange = 0;
-    testTime = ESP8266TrueRandom.random(11,60*60*2);
-
-    bool status;
-    //gScript_type = hb;
-    Irms = ESP8266TrueRandom.random(10,600)/100.0;
-
-    if(Irms>current_factor_threshold_for_motor_ON)
+    if(true==whether_post_wifi_connect_setup_done)
     {
-      status = true ; //signifies motor is ON
+      if(WiFi.status() == WL_CONNECTED)
+      {
+        if(loop_OTA())
+        {
+          return;
+        }
+        rd_loop();
+      }
     }
 
-    if(Irms<current_factor_threshold_for_motor_ON)
+
+    bool state = curretSample_Loop(&Irms);
+   state = curretSample_Loop(&Irms);
+   state = curretSample_Loop(&Irms);
+ state = curretSample_Loop(&Irms);
+
+
+
+
+ if(millis()-last_time_telnet_talked>updateTelnetInterval)
+ {
+   last_time_telnet_talked = millis();
+   DEBUG_V("* Irms: %f A (VERBOSE)\n", Irms);
+}
+
+ if(millis()-last_time_thingspoke>updateThingSpeakInterval) // && samples.getCount() == samples.getSize())
+ {
+
+  last_time_thingspoke = millis();
+
+  if(WiFi.status() == WL_CONNECTED)
+  {
+    Serial.print("Wifi connection OK ");
+    Serial.printf("IP %s\n", WiFi.localIP().toString().c_str());
+
+    if (client.connect(server,80))
     {
 
-      status = false ; //signifies motor is OFF
+      String tsData = apiWritekey;
+      tsData +="&field1=";
+      tsData += String(Irms); // filtered time_us
+      tsData +="&field2=";
+      tsData += String(millis()*0.001);
+      tsData += "\r\n\r\n";
+
+      client.print("POST /update HTTP/1.1\n");
+      client.print("Host: api.thingspeak.com\n");
+      client.print("Connection: close\n");
+      client.print("X-THINGSPEAKAPIKEY: "+String(apiWritekey)+"\n");
+      client.print("Content-Type: application/x-www-form-urlencoded\n");
+      client.print("Content-Length: ");
+      client.print(tsData.length());
+      client.print("\n\n");
+      client.print(tsData);
+      Serial.print("ThingSpeak data sent");
+
     }
-
-    state = status;
-
-    //state = (bool)ESP8266TrueRandom.randomBit();
-
-
-    Serial.print("Random test ts:");
-    Serial.print(testTime*1000+millis());
-    Serial.print(" test Irms: ");
-    Serial.println(Irms);
-  }
-
-  #endif // !defined(TEST_MODE)
-
-  if(!isWifiConnected)
-  {
-    Serial.println();
-
-    //googlespreadsheet_keepready();
-    // Block check for internet too
-    checkInternetConnection();
-
-    isWifiConnected = true; // set it true as googlespreadsheet_keepready() will make it online
-
-    if(gScript_type == unknown)
+    else
     {
-      gScript_type = reConnected;
-      ts = millis()-lastDisconnectionTs;
-      if(DISCONNECTION_TIME<ts)
-      //googlespreadsheet_Loop(motor_status_gScript, Irms, gScript_type, ts);
-      phpgsheet_Loop(motor_status_gScript, Irms, gScript_type, ts);
+
     }
+    client.stop();
+   }
+ }
 
-    //googlespreadsheet_Loop(motor_status_gScript, Irms, gScript_type, ts );
-  }
 
-  // Motor status
 
-  if( UNKNOWN == motor_status_gScript )
-  {
 
-    lastMotorStatusChangeTs = millis();
-
-  }
-
-  motor_status_gScript = state==true ? ON : OFF;
-
-  if(last_motor_status_gScript != motor_status_gScript)
-  {
-    //if(motorStatusChange > 10000)
-    //{
-    //motorStatusChange = 0;
-    //if(gScript_type == unknown)
-
-    last_motor_status_gScript = motor_status_gScript;
-
-    //Serial.println("State changes");
-
-    if( (gScript_type == unknown) || (gScript_type == reConnected) )
-    {
-      gScript_type = motorStats;
-      ts = millis()-lastMotorStatusChangeTs;
-
-      Serial.print("Motor ts:");
-      Serial.println(ts);
-
-      lastMotorStatusChangeTs = millis();
-      //googlespreadsheet_Loop(motor_status_gScript, Irms, gScript_type, ts);
-      phpgsheet_Loop(motor_status_gScript, Irms, gScript_type, ts);
-    }
-
-    //}
-  }
-
-  // hb
-  if( (gScript_type == unknown) || (gScript_type == motorStats) )
-  {
-    gScript_type = hb;
-    ts = millis() ;
-
-    //sendAction();
-    //googlespreadsheet_Loop(motor_status_gScript, Irms, gScript_type, ts);
-    if(3<phpgsheet_Loop(motor_status_gScript, Irms, gScript_type, ts))
-    {
-      ESP.restart(); // Work around no time for solving connection drop.
-    }
-  }
 }
